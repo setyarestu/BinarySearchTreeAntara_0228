279757be5cf09a2800b26f11c750b2f9b9dc68b5
setyarestu <setyarestunugroho@gmail.com>
2026-08-25 13:43:45 +0700

    prosedur search

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -62,4 +62,19 @@ public :
         }
     }
 
+    //membuat dan mendefinisikan prosedur search
+    void search(string element, Node*& parent, Node*& currentNode) {
+        currentNode = ROOT;
+        parent = nullptr;
+        while ((currentNode != nullptr) && (currentNode->info != element))
+        {
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftChild;
+            else
+                currentNode = currentNode->rightChild;
+
+        }
+    }
+
    
