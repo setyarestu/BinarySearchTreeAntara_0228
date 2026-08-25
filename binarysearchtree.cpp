5d3fd9bdb17f7b2e19d86de63ea7c006d8ceb259
setyarestu <setyarestunugroho@gmail.com>
2026-08-25 13:44:40 +0700

    preorder

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -91,3 +91,19 @@ public :
         }
     }
 
+    //membuat dan mendefinisikan prosedur preorder
+    void preorder(Node* ptr) {
+
+        if (ROOT == nullptr) {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != nullptr) {
+            cout << ptr->info << " ";
+            preorder(ptr->leftChild);
+            preorder(ptr->rightChild);
+        }
+
+    }
+
+    
