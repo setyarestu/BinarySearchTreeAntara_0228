10d22cdfe3c2f8899a988a58a1aff378ee60895e
setyarestu <setyarestunugroho@gmail.com>
2026-08-25 13:39:47 +0700

    modifier

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -4,4 +4,10 @@ using namespace std;
 //membuat class node
 class Node {
 
-   
+    //memberi akses modifier serta mendeklarasi variabel dan object node
+public :
+    string info;
+    Node* leftChild;
+    Node* rightChild;
+
+    
