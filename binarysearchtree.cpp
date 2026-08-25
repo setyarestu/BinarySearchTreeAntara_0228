019b810be2c9e5520165f0c7c7af93ef065f6ad9
setyarestu <setyarestunugroho@gmail.com>
2026-08-25 13:40:40 +0700

    memberi nilai para

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -12,4 +12,12 @@ public :
 
     //membuat constructor untuk class node
     Node(string i, Node* l, Node* r) {
-       
+        //memberi nilai parameter ke variabel
+        info = i;
+        leftChild = l;
+        rightChild = r;
+    }
+
+};
+
+
